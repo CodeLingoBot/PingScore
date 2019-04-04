bd0dac2aee36c87f59d671e057ce42612b1f214f
luc@intech.nimes <l.nicolas@intech-sud.fr>
2019-04-04 19:26:30 +0200

    database + espace competition

---
+++ controllers/database.php
@@ -0,0 +1,10 @@
+<?php
+try {
+    $strConnexion = 'mysql:host=localhost;dbname=PI_aspcn' ;
+    $pdo = new PDO ($strConnexion,'root','root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e){
+    $msg = 'Erreur PDO dans ' . $e -> getMessage();
+    die($msg);
+}
---
+++ views/panel/espace_competition.php
@@ -0,0 +1,55 @@
+<?php
+require_once '../../includes/functions.php';
+logged();
+
+include_once '../../includes/partials/header_panel.php';
+?>
+
+<script>
+    document.getElementById("body").style.background = "url('https://cwsmgmt.corsair.com/newscripts/landing-pages/wallpaper/v4/Wallpaper-v4-1920x1200.jpg') no-repeat center center fixed";
+</script>
+    <!-- Page Content -->
+    <div class="container">
+        <div class="card border-0 shadow my-5">
+            <div class="card-body p-5">
+                <h1 class="font-weight-light text-primary">ESPACE COMPETITION</h1>
+                <p class="lead">Voici l'espace de gestion de la compétition, ici vous pouvez accéder aux différents espace matchs, joueurs et arbitres</p>
+
+                <table>
+                    <td>
+                        <div class="card border-0 shadow my-5 btn-light">
+                            <a href="liste_matchs.php">
+                                <div class="card-body p-5">
+                                    <h1 class="font-weight-light text-success">Liste des matchs</h1>
+                                    <p class="lead">Accès à la liste des matchs</p>
+                            </div></a>
+                        </div>
+                    </td>
+
+                    <td>
+                        <div class="card border-0 shadow my-5 btn-light">
+                            <a href="liste_joueurs.php"><div class="card-body p-5">
+                            <h1 class="font-weight-light text-danger">Liste des joueurs</h1>
+                            <p class="lead">Accès à la liste des joueurs</p>
+                        </div></a>
+                    </div>
+                    </td>
+
+                    <td>
+                        <div class="card border-0 shadow my-5 btn-light">
+                            <a href="liste_arbitres.php"><div class="card-body p-5">
+                                <h1 class="font-weight-light text-warning">Liste des arbitres</h1>
+                                <p class="lead">Accès à la liste des arbitres</p></a>
+                        </div>
+                    </div>
+                    </td>
+
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+<?php
+include '../../includes/partials/footer_panel.php'
+?>
