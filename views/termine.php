9780f5e96526deae4bbb43bbdea1e0a7d0fa80bd
Marti <h.marti@intech-sud.fr>
2019-04-26 13:07:16 +0200

    [VUE] match terminé de nouveau fonctionnel

--- views/termine.php
+++ views/termine.php
@@ -10,7 +10,7 @@
 
     <?php
         require_once ('../controllers/database.php');
-        $req = "SELECT m.court, j1.surname AS blue, j1.cat AS blue_cat, j2.surname AS red, j2.cat AS red_cat, m.hour, m.score FROM matchs m INNER JOIN players j1 ON (j1.id = m.blue_player) INNER JOIN players j2 ON (j2.id = m.red_player) WHERE (CONVERT(`state` USING utf8) LIKE '%2%') ORDER BY m.`hour` DESC";
+        $req = "SELECT m.*,  j1.surname AS j1_surname, j1.name AS j1_name, j1.cat AS j1_cat, j2.surname AS j2_surname, j2.name AS j2_name, j2.cat AS j2_cat FROM matchs m INNER JOIN players j1 ON (j1.id = m.blue_player) INNER JOIN players j2 ON (j2.id = m.red_player) WHERE m.state = 2";
         $ps = $pdo -> prepare($req);
         $ps -> execute();
 
@@ -23,17 +23,17 @@
             <table class="table table-secondary">
 
                 <thead>
-                    Table <?php echo($et['court'])?> - <?php echo($et['hour'])?>
-                    <?php if ($et['blue_cat'] == $et['red_cat']) { 
-                        $cat = $et['blue_cat'] ; 
+                    <!--Table <?php #echo($et['court'])?> - --><?php echo($et['hour'])?>
+                    <?php if ($et['j1_cat'] == $et['j2_cat']) { 
+                        $cat = $et['j1_cat'] ; 
                         echo(" - Cat. ".$cat) ; 
                     } ?>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="row">
-                            <?php echo($et['blue'])?>
-                            <?php if ($et['blue_cat'] != $et['red_cat']) { $cat = $et['blue_cat']; echo( "  Cat. ".$cat); } ?>
+                            <?php echo($et['j1_name'].' '.$et['j1_surname'])?>
+                            <?php if ($et['j1_cat'] != $et['j2_cat']) { $cat = $et['j1_cat']; echo( "  Cat. ".$cat); } ?>
                         </th>
                         <td width=10%><?php echo($json->round1->blue)?></td>
                         <td width=10%><?php echo($json->round2->blue)?></td>
@@ -43,8 +43,8 @@
                     </tr>
                     <tr>
                         <th scope="row">
-                            <?php echo($et['red'])?>
-                            <?php if ($et['red_cat'] != $et['blue_cat']) { $cat = $et['red_cat']; echo( "  Cat. ".$cat); } ?>
+                        <?php echo($et['j2_name'].' '.$et['j2_surname'])?>
+                        <?php if ($et['j1_cat'] != $et['j2_cat']) { $cat = $et['j2_cat']; echo( "  Cat. ".$cat); } ?>
                         </th>
                         <td width=10%><?php echo($json->round1->red)?></td>
                         <td width=10%><?php echo($json->round2->red)?></td>
