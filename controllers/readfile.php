f5daea6c740d9ec89c747a7385218af417761d14
luc@intech.nimes <l.nicolas@intech-sud.fr>
2019-05-08 00:39:08 +0200

    :sparkles: [PANEL] mise en place de la création de la compétition
    l'admin peut mettre en place la compétition en entrant le nombre de tables ainsi que le fichier CSV avec toutes les données joueurs

---
+++ controllers/readfile.php
@@ -0,0 +1,67 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\Csv;
+
+$courts = $_POST['courts'];
+$i = 0;
+
+$file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+if(isset($_FILES['file']['name']) && in_array($_FILES['file']['type'], $file_mimes)) {
+
+    $arr_file = explode('.', $_FILES['file']['name']);
+    $extension = end($arr_file);
+
+    if('csv' == $extension) {
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+    }
+
+    $spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+
+    $sheetData = $spreadsheet->getActiveSheet()->toArray();
+
+
+    require_once 'database.php';
+
+    $reqBase = "
+            TRUNCATE `lucnicol_aspcn`.`court`;
+            TRUNCATE `lucnicol_aspcn`.`players`;
+            TRUNCATE `lucnicol_aspcn`.`matchs`;
+            ";
+    $insertBase = $pdo->prepare($reqBase);
+    $insertBase->execute();
+    $insertBase->closeCursor();
+
+
+    while ($i < $courts) {
+        $reqCourt = "INSERT INTO `court` (`match_id`, `video`) VALUES (NULL, '')";
+        $insertCourt = $pdo->prepare($reqCourt);
+        $insertCourt->execute();
+        $insertCourt->closeCursor();
+        $i++;
+    }
+
+    $sheetData_lenght = count($sheetData);
+
+    for ($i = 1; $i < $sheetData_lenght; $i++) {
+        $surname = $sheetData[$i][0];
+        $name = $sheetData[$i][1];
+        $cat = $sheetData[$i][2];
+        $club = $sheetData[$i][3];
+        $rank = $sheetData[$i][4];
+
+        $reqQuery = 'INSERT INTO players(surname, name, cat, club, rank) VALUES(:surname, :name, :cat, :club, :rank);';
+        $query = $pdo->prepare($reqQuery);
+        $query->bindValue(":surname", $surname);
+        $query->bindValue(":name", $name);
+        $query->bindValue(":cat", $cat);
+        $query->bindValue(":club", $club);
+        $query->bindValue(":rank", $rank);
+        $query->execute();
+    };
+}
+
+header('Location: ../views/panel/espace_competition.php');
--- views/panel/espace_competition.php
+++ views/panel/espace_competition.php
@@ -6,8 +6,12 @@ check_auth();
 
     require_once('../../controllers/database.php') ;
 
-    $court = $pdo->prepare("SELECT id FROM court") ;
-    $court->execute() ;
+    try {
+        $court = $pdo->query("SELECT * FROM court") ;
+    } catch (Exception $e){
+        echo 'Message: ' .$e->getMessage();
+    }
+
 
     $matchs = $pdo->prepare("SELECT * FROM matchs WHERE state=0 ORDER BY hour ASC") ;
     $matchs->execute() ;
@@ -16,7 +20,11 @@ check_auth();
 
     include_once '../../includes/partials/header_panel.html';
     include_once '../../includes/toasts.php';
+
 ?>
+
+<?php if ($data = $court->fetchAll()) : ?>
+
     <!-- Page Content -->
     <div class="container">
         <div class="card border-0 shadow my-5">
@@ -62,5 +70,32 @@ check_auth();
             </div>
         </div>
     </div>
+
+<?php else : ?>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="card border-0 shadow my-5">
+            <div class="card-body p-5">
+                <h1 class="font-weight-light text-primary">ESPACE COMPETITION</h1>
+                <p class="lead">Voici l'espace de gestion de la compétition, ici vous pouvez accéder aux différents espace matchs, joueurs et arbitres</p>
+
+                <form method="post" enctype="multipart/form-data" action="../../controllers/readfile.php">
+                    <div class="form-group col-md-4 mb-3">
+                        <input type="text" name="courts" class="form-control" id="aaa">
+                        <label for="aaa">Nombre de table</label>
+                    </div>
+                    <div class="custom-file col-md-4 mb-3">
+                        <input type="file" name="file" class="custom-file-input" id="InputFile">
+                        <label class="custom-file-label" for="InputFile">Importer la base de donnée</label>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<?php endif; ?>
 
 <?php include '../../includes/partials/footer_panel.html' ?>
