5ca399c367f99f1ba07c14b2b280dd236ddc3c74
luc@intech.nimes <l.nicolas@intech-sud.fr>
2019-05-08 01:38:10 +0200

    :bug: [PANEL] correction bug identifiant root après rest
    problème simple/double cotes ...

--- controllers/reset.php
+++ controllers/reset.php
@@ -2,15 +2,13 @@
 
 require_once 'database.php';
 
-$req = "
+$req = '
     SET FOREIGN_KEY_CHECKS=0;
     TRUNCATE `lucnicol_aspcn`.`court`; 
     TRUNCATE `lucnicol_aspcn`.`matchs`; 
     TRUNCATE `lucnicol_aspcn`.`players`; 
     TRUNCATE `lucnicol_aspcn`.`users`;
     SET FOREIGN_KEY_CHECKS=1;
-    INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES (1, 'root', '$2y$10$D2Oajot8QdRoTy7Qcyklf.VW2tp8kDGP79jQL7rimi11Y8SoUVVL2', 'admin');
-    ";
+    INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES (1, "root", "$2y$10$D2Oajot8QdRoTy7Qcyklf.VW2tp8kDGP79jQL7rimi11Y8SoUVVL2", "admin");';
 $data = $pdo->query($req);
-
 header('Location: ../views/panel/espace_competition.php');
