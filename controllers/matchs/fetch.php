8e6dfa7616a00ad4fc4936a6284c5651f52f7186
luc@intech.nimes <l.nicolas@intech-sud.fr>
2019-05-09 00:18:26 +0200

    :children_crossing: [PANEL] modification du format de l'heure des matchs (%H:%i)

--- controllers/matchs/fetch.php
+++ controllers/matchs/fetch.php
@@ -4,7 +4,7 @@ require_once '../database.php';
 $id = $_POST["id"];
 
 if(isset($id)){
-    $req = "SELECT * FROM matchs WHERE id=?";
+    $req = "SELECT id, TIME_FORMAT(hour, '%H:%i') AS hour, blue_player, red_player, state FROM matchs WHERE id=?";
     $result = $pdo->prepare($req);
     $result->execute(array($id));
     $row = $result->fetch();
