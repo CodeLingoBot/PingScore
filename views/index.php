08b93395a306a8e5f603756e8a02c21eee5e518a
Marti <h.marti@intech-sud.fr>
2019-05-09 11:00:22 +0200

    :alembic: [VUE] création page index
    Le dites pas à Luc svp

---
+++ views/index.php
@@ -0,0 +1,11 @@
+<!--==========-->
+
+<?php include("../includes/partials/header.html") ?>
+
+<!--==========-->
+
+<div class="container">
+
+    <img src="../assets/img/logo_ping_score.svg" class="img-fluid" alt="Responsive image">
+
+</div>
